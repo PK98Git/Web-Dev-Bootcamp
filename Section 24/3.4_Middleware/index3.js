5e65cbe2f9193272d9c8e73332dc952f7a8f8a95
PasinduK98 <pkbiz1998@gmail.com>
2023-09-24 19:06:41 +0530

    Custom MIddelware

--- Section 24/3.4_Middleware/index3.js
+++ Section 24/3.4_Middleware/index3.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req,res,next){
+  console.log("request Method :",req.method);//This will log  type of method of the request
+  console.log("request URL :", req.url);
+  next();
+  //Without next function it will not move to the other lines 
+}
 app.use(logger);
 
 app.get("/", (req, res) => {
