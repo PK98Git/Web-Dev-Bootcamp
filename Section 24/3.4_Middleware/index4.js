27ae41dec327cd929a914ab415e58da26300c632
PasinduK98 <pkbiz1998@gmail.com>
2023-09-24 21:58:51 +0530

    Added Custom Middleware

--- Section 24/3.4_Middleware/index4.js
+++ Section 24/3.4_Middleware/index4.js
@@ -1,8 +1,33 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+var bandName = '';
+
+//use this to pass encoded type data through the url
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bandNameGen);
+
+function bandNameGen(req,res,next){
+  bandName = req.body["street"]+req.body["pet"];
+  next();
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+//Create Post request to submit the data
+app.post("/submit",(req,res)=>{
+  res.send(`Your band name is :<h1>${bandName}<h1>`);
+
+})
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
