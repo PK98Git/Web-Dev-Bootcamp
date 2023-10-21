da7868864c4ecd473c6feb44ec1264a99d900c7f
PasinduK98 <pkbiz1998@gmail.com>
2023-10-21 20:38:27 +0530

    feat:activities filter on type

--- Section 28/5.3+Axios/index.js
+++ Section 28/5.3+Axios/index.js
@@ -16,7 +16,7 @@ app.get("/", async (req, res) => {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
     //const random = Math.round(Math.random()*result.length())
-    console.log(result)
+    console.log(result);
     res.render("index.ejs", { data: result });
   } catch (error) {
     console.error("Failed to make request:", error.message);
@@ -29,40 +29,49 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   //console.log(req.body);
-  try{
-  let response;
-  let result;
-  let random;
+  try {
+    let response;
+    let result;
+    let random;
 
-  if(req.body.type&&req.body.participants){
-  console.log("type and parti")
+    if (req.body.type && req.body.participants) {
+      console.log("type and parti");
 
-        response = await axios.get("https://bored-api.appbrewery.com/filter?type="+req.body.type+"&participants="+req.body.participants);
-        result = response.data;
-        random = Math.round(Math.random() * response.data.length);
-        //console.log(random);
-        //console.log(response.data.length)
-        res.render("index.ejs", { data: result[random] });
-  }else if(req.body.type){
-    console.log("type only");
-
-  }
-  else if(req.body.participants){
-    console.log("Participants only")
-
-  }else{
-    console.log("Random");
-
-  }
-    } catch (error) {
+      response = await axios.get(
+        "https://bored-api.appbrewery.com/filter?type=" +
+          req.body.type +
+          "&participants=" +
+          req.body.participants
+      );
+      result = response.data;
+      random = Math.round(Math.random() * response.data.length);
+      //console.log(random);
+      //console.log(response.data.length)
+      res.render("index.ejs", { data: result[random] });
+    } else if (req.body.type) {
+      console.log("type only");
+      response = await axios.get(
+        "https://bored-api.appbrewery.com/filter?type=" + req.body.type
+      );
+      result = response.data;
+      random = Math.round(Math.random() * response.data.length);
+      //console.log(random);
+      //console.log(response.data.length)
+      res.render("index.ejs", { data: result[random] });
+    } else if (req.body.participants) {
+      console.log("Participants only");
+    } else {
+      console.log("Random");
+    }
+  } catch (error) {
     //console.error("Failed to make request:", error.message);
-    console.log("error Message",error);
+    console.log("error Message", error);
     // if (error.response.status)
     //   res.render("index.ejs", {
     //     error: error
     //   });
   }
-  
+
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
   // sure you're passing both the type and participants queries.
