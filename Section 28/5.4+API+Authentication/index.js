be5212cf7cb7a6b689c4b0876a04a1a7e11fd0e4
PasinduK98 <pkbiz1998@gmail.com>
2023-10-25 20:56:28 +0530

    add API endpoints for No Auth and Basic Auth

--- Section 28/5.4+API+Authentication/index.js
+++ Section 28/5.4+API+Authentication/index.js
@@ -66,18 +66,7 @@ app.get("/apiKey", async (req, res) => {
   //Filter for all secrets with an embarassment score of 5 or greater
   //HINT: You need to provide a query parameter of apiKey in the request.
 
-try {
-  const response = await axios.get(
-    "https://secrets-api.appbrewery.com/filter",{
-      params:{
-        score:5,
-        apiKey:yourAPIKey,
-      },
-    });
-  res.render("index.ejs", { content: JSON.stringify(response.data) });
-} catch (error) {
-  res.status(404).send(error.message);
-}
+
 });
 
 app.get("/bearerToken", (req, res) => {
